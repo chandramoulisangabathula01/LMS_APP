3505e27eb4219efc1274efdf25a41427aa0737cc
chandramoulisangabathula01 <chandramoulisangabathula01@gmail.com>
2024-01-30 22:51:48 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', function (request, response) {
+    response.send('Hello World')
+  })
+
+
+  app.listen(3000,() => {
+    console.log("started at 3000")
+  })
