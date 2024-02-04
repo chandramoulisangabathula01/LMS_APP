9bd11003e1202b26717d724e9c5605d5400a1b14
chandramoulisangabathula01 <chandramoulisangabathula01@gmail.com>
2024-02-04 21:10:31 +0530

    deploy change in pkg.json

--- migrations/20240131064159-create-courses.js
+++ migrations/20240131064159-create-courses.js
@@ -17,15 +17,6 @@ module.exports = {
       courseContent: {
         type: Sequelize.TEXT
       },
-      // created_at: {
-      //   allowNull: false,
-      //   defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      //   type: Sequelize.DATE
-      // },
-      // updated_at: {
-      //   allowNull: false,
-      //   type: Sequelize.DATE
-      // },
       createdAt: {
         allowNull: false,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
