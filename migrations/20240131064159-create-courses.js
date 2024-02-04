058433afd823fcec16640c0686d5629b4cb57058
chandramoulisangabathula01 <chandramoulisangabathula01@gmail.com>
2024-02-04 11:53:53 +0530

    deploy issue try 5,Crt_at

--- migrations/20240131064159-create-courses.js
+++ migrations/20240131064159-create-courses.js
@@ -17,20 +17,20 @@ module.exports = {
       courseContent: {
         type: Sequelize.TEXT
       },
-      // created_at: {
-      //   allowNull: false,
-      //   defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      //   type: Sequelize.DATE
-      // },
-      // updated_at: {
-      //   allowNull: false,
-      //   type: Sequelize.DATE
-      // },
-      // createdAt: {
-      //   allowNull: false,
-      //   defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      //   type: Sequelize.DATE
-      // },
+      created_at: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        type: Sequelize.DATE
+      },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
